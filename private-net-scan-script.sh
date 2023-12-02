14d422e0094ef3cd57fa52d434eed61c18255ca3
im-kb <kamil.budzik98@gmail.com>
2023-12-02 02:00:00 +0100

    improve net scan script

--- private-net-scan-script.sh
+++ private-net-scan-script.sh
@@ -2,12 +2,28 @@
 
 RPC_ENDPOINT="http://localhost:8545"
 
-# Get the latest block number
-latestBlockNumber=$(curl -s -X POST --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' -H "Content-Type: application/json" $RPC_ENDPOINT | grep -oP '(?<="result": ")[^"]*')
+# Get the current date and time in a format suitable for filenames
+currentDateTime=$(date +"%Y%m%d_%H%M%S")
 
-# Iterate through blocks and print basic block information
-for ((i=0; i<=16 ${latestBlockNumber}; i++)); do
-    blockInfo=$(curl -s -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["0x'"$(printf "%x" $i)"'", true],"id":1}' -H "Content-Type: application/json" $RPC_ENDPOINT)
+# Get the latest block number in hexadecimal format
+hexLatestBlockNumber=$(curl -s -X POST --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' -H "Content-Type: application/json" $RPC_ENDPOINT | grep -oP '(?<="result":"0x)[^"]*')
 
-    echo -e "Block $i Information:\n${blockInfo//$'\\n'/\\n}\n"
+# Convert hexadecimal to decimal using Bash arithmetic
+latestBlockNumber=$((16#${hexLatestBlockNumber}))
+
+# Define the output file with the current date and time
+outputFile="block_info_${currentDateTime}.txt"
+
+# Iterate through blocks and print basic block information, saving to the output file
+for ((i=0; i<=latestBlockNumber; i++)); do
+    hexBlockNumber=$(printf "%x" $i)
+    blockInfo=$(curl -s -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["0x'"$hexBlockNumber"'", true],"id":1}' -H "Content-Type: application/json" $RPC_ENDPOINT)
+
+    # Prepend the "Block Information" to the JSON result
+    blockInfo="{\"BlockId\":\"$i\", ${blockInfo#*\{}"
+
+    # Append the block information to the output file
+    echo "$blockInfo" >> "$outputFile"
 done
+
+echo "Output saved to: $outputFile"
